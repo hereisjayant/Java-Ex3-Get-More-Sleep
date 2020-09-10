b5387e391969cd4fc7862253a18d27eca9c26208
hereisjayant <hereisjayant@gmail.com>
2020-09-10 08:58:58 +0530

    Made some changes in the source

--- src/main/java/spiralizer/MatrixSpiralizer.java
+++ src/main/java/spiralizer/MatrixSpiralizer.java
@@ -14,10 +14,60 @@ public class MatrixSpiralizer {
         int rows = inMatrix.length;
         int cols = inMatrix[0].length;
 
-
-
         outMatrix = new int[rows][cols];
 
-        return outMatrix; // CHANGE THIS!
+        //Make the traverser go through n elements before changing the way
+        int r= rows; //these are the ns of our program
+        int c= cols;
+
+        int numberOfElements = rows*cols;
+
+        int nth_element= 0;
+
+        int i=0;
+        int j=0;
+
+
+        while(true) {
+            for (; j <= c-1; j++) { //here i is fixed, and j is changing
+
+                //this gets the position of the input matrix
+                int row_in = getCurrentRow(nth_element, rows);
+                int col_in = getCurrentCol(nth_element, cols);
+
+                // places input matrix element to output matrix
+                outMatrix[i][j]= inMatrix[row_in][col_in];
+
+                //increases the nth element counter in inMatrix
+                nth_element++;
+                //TODO: change c here
+            }
+            for (; i <= r-1; i++) {
+
+                //this gets the position of the input matrix
+                int row_in = getCurrentRow(nth_element, rows);
+                int col_in = getCurrentCol(nth_element, cols);
+
+                // places input matrix element to output matrix
+                outMatrix[i][j]= inMatrix[row_in][col_in];
+
+                //increases the nth element counter in inMatrix
+                nth_element++;
+                //TODO: change r here
+            }
+        }
+        return outMatrix;
     }
+
+
+    private static int getCurrentRow(int n, int rows){
+
+        return n/rows;
+    }
+
+    private static int getCurrentCol(int n, int cols){
+
+        return n/cols;
+    }
+
 }
